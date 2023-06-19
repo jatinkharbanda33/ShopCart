5a5d8c12040a0f241b497c5d5d86beb1cecf9c42
Jatin Kharbanda <jatinkharbanda33@gmail.com>
2023-06-20 02:24:20 +0530

    Add file via commit

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,20 @@
+import 'package:theshopapp/models/grocery_item.dart';
+import 'package:theshopapp/data/categories.dart';
+import '../models/category.dart';
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Groceries"),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            height: 24,
+            width: 24,
+            color: groceryItems[index].category.itscolor,
+          ),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
+      ),
+    );
+  }
+}
